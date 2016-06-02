461447142398df544034877868b5b4a1bc5a3144
Shapeshift-Public <mail@shapeshift.io>
2016-06-02 16:55:35 -0600

    initial commit

---
+++ dev-server.js
@@ -0,0 +1,16 @@
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static')
+
+// Serve up public/ftp folder
+var serve = serveStatic(__dirname, {'index': ['index.html', 'index.htm']})
+
+// Create server
+var server = http.createServer(function(req, res){
+    var done = finalhandler(req, res)
+    serve(req, res, done)
+})
+
+// Listen
+server.listen(3000)
+console.log("Server Listening On Port 3000\n   type 'http://localhost:3000' into browser");
